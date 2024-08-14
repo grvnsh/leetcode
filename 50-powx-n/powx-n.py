ceda73772aac51a92b4ba19e8359d45513b8e2ac
Gurvansh Gill <gurvanshsgill@gmail.com>
2024-08-13 23:47:35 -0400

    Time: 13 ms (63.96%) | Memory: 11.6 MB (38.18%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -2,20 +2,15 @@ class Solution(object):
     def myPow(self, x, n):
         if n == 0:
             return 1.0
-        
-        N = n
-        if N < 0:
+        absN = n
+        if absN < 0:
             x = 1 / x
-            N = -N
-        
+            absN = -absN
         result = 1.0
         prod = x
-        
-        while N > 0:
-            if N % 2 == 1:
+        while absN > 0:
+            if absN % 2 == 1:
                 result *= prod
             prod *= prod
-            N //= 2
-        
+            absN //= 2
         return result
-        
