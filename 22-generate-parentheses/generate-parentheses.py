b862e8b49b0abe42be3c58a3750f28e6c7721dd2
Gurvansh Gill <gurvanshsgill@gmail.com>
2024-08-15 23:59:55 -0400

    Time: 22 ms (23.92%) | Memory: 11.7 MB (88.73%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        def brackets(l, r, str):
+		if len(str) == n * 2:
+			result.append(str)
+			return 
+
+		if l < n:
+			brackets(l + 1, r, str + '(')
+
+		if r < l:
+			brackets(l, r + 1, str + ')')
+
+	result = []
+	brackets(0, 0, '')
+	return result
