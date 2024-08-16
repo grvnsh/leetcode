2316129a827ac28371cc0f82d699dcb2f96458c4
Gurvansh Gill <gurvanshsgill@gmail.com>
2024-08-16 00:01:06 -0400

    Time: 14 ms (80.18%) | Memory: 11.9 MB (62.09%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -8,13 +8,10 @@ class Solution(object):
 		if len(str) == n * 2:
 			result.append(str)
 			return 
-
 		if l < n:
 			brackets(l + 1, r, str + '(')
-
 		if r < l:
 			brackets(l, r + 1, str + ')')
-
 	result = []
 	brackets(0, 0, '')
 	return result
