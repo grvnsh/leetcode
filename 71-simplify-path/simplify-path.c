c7a92ed7a05dcdb47d843d364ed3a1d0e832a31b
Gurvansh Gill <gurvanshsgill@gmail.com>
2024-08-27 15:39:25 -0400

    Time: 0 ms (100.00%) | Memory: 8.3 MB (77.73%) - LeetSync

--- 71-simplify-path/simplify-path.c
+++ 71-simplify-path/simplify-path.c
@@ -3,13 +3,13 @@ char* simplifyPath(char* path) {
     int n = strlen(path);
     for (r = 0; r <= n; r++) {
         if (r == n || path[r] == '/') {
-            if (l == 0) {
-                continue;
-            } else if (l == 1 && path[r-1] == '.') {
+            if (l == 0 || (l == 1 && path[r-1] == '.')) {
                 l = 0;
             } else if (l == 2 && path[r-2] == '.' && path[r-1] == '.') {
-                while (w > 0 && path[w-1] != '/') w--;
-                if (w > 0) w--;
+                if (w > 0) {
+                    w--;
+                    while (w > 0 && path[w-1] != '/') w--;
+                }
                 l = 0;
             } else {
                 if (w == 0 || path[w-1] != '/') {
@@ -25,6 +25,8 @@ char* simplifyPath(char* path) {
     }
     if (w == 0) {
         path[w++] = '/';
+    } else if (w > 1 && path[w-1] == '/') {
+        w--;
     }
     path[w] = '\0';
     return path;
