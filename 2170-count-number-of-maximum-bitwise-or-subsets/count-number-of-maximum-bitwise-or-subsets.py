3e8bfd1cf1f9d0aaf89c5cce02f0af4bd4864341
Gurvansh Gill <gurvanshsgill@gmail.com>
2024-10-18 23:58:52 -0400

    Time: 330 ms (84.62%) | Memory: 11.6 MB (88.46%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
@@ -7,14 +7,10 @@ class Solution(object):
         max_or = 0
         for num in nums:
             max_or |= num
-        
         def back(i, curr):
             if i == len(nums):
                 return 1 if curr == max_or else 0
-            
             in_ = back(i + 1, curr | nums[i])
             out = back(i + 1, curr)
-            
             return in_ + out
-        
         return back(0, 0)
